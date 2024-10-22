90489e52519ff9cdee1c3313c4bae27406adc3be
Juicy1011 <juicyplanet1@gmail.com>
2024-10-22 21:54:16 +0300

    with backend configured

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: './drizzle',
+  schema: './db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url:'postgresql://Expense%20tracker%20app_owner:b5klV0fJUAsB@ep-still-surf-a51wled8.us-east-2.aws.neon.tech/Expense%20tracker%20app?sslmode=require',
+  },
+});
